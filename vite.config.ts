94cdf51204707e933a2ac0e0d8293d62bfb42023
yandyphotobooth-ux <yandyphotobooth@gmail.com>
2026-02-25 02:12:34 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env': {
+        GEMINI_API_KEY: env.GEMINI_API_KEY || '',
+        NODE_ENV: mode,
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      hmr: process.env.DISABLE_HMR !== 'true',
+    },
+  };
+});
